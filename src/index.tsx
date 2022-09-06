3de85956dd4c9201536bebd07742a96382518e98
LGretzk <LGretzk@protonmail.com>
2022-09-06 19:23:13 +0100

    Add BrowserRouter and Provider to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './app/store'
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <Provider store={store}>
+              <App />
+        </Provider>
+    </BrowserRouter>
 );
 
 
