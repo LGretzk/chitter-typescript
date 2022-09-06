49cd2d825c1e337f4cf7b0a2db2336a98e540c0a
LGretzk <LGretzk@protonmail.com>
2022-09-06 19:18:13 +0100

    Update App.tsx with the peeps route (page)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
 import LoginForm from '../components/LoginForm/LoginForm'
 import RegistrationForm from '../components/RegistrationForm/RegistrationForm';
+import Peeps from '../routes/Peeps/Peeps'
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <LoginForm />
-      <RegistrationForm />
-    </div>
-  )
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/peeps" element={<Peeps />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App;
