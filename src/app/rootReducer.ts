897363ef34d0055a90ae62c6a712156dc17737c1
LGretzk <LGretzk@protonmail.com>
2022-09-06 18:35:50 +0100

    Update root reducer by adding peeps reducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
+import peepReducer from '../store/peeps/Peeps.reducers'
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+    peeps: peepReducer
+});
 
 export type RootState = ReturnType<typeof rootReducer>;
 
