f6d735380e810c97c501bfbb22ea7d14d214d8b0
LGretzk <LGretzk@protonmail.com>
2022-09-07 10:40:09 +0100

    Create useAppDispatch and useAppSelector hooks in the hooks.ts

--- src/routes/Peeps/Peeps.tsx
+++ src/routes/Peeps/Peeps.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch } from "../../app/store";
 import { loadPeeps } from "../../store/peeps/Peeps.actions";
-import { RootState } from "../../app/rootReducer";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 
 const Peeps: React.FC = () => {
-    const dispatch: AppDispatch = useDispatch();
-    let peeps = useSelector((state: RootState) => state.peeps);
+    const dispatch = useAppDispatch();
+    // changes after adding the hooks.ts
+    // const dispatch: AppDispatch = useDispatch();
+    let peeps = useAppSelector(state => state.peeps);
+    // let peeps = useSelector((state: RootState) => state.peeps);
 
     useEffect(() => {
         async function load() {
